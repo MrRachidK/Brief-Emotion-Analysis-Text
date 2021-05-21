bc30e1a3dff5e57be313d1eea636c8c315df05ca
Kevinb-a <kevin.billet.pro@gmail.com>
2021-05-21 14:38:15 +0200

    neural network finish

--- src/app/streamlit_main.py
+++ src/app/streamlit_main.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,'/home/apprenant/Documents/Brief-Emotion-Analysis-Text')
+sys.path.insert(0,'/home/apprenant/simplon_project/Brief-Emotion-Analysis-Text')
 import streamlit as st # type: ignore
 from src.app import app1
 from src.app import app2 
